532b50e353b510fb2e5f1f27ad2226c5cf07db63
Bruce-Stark <1052882558@qq.com>
2021-09-13 22:14:12 +0800

    test

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,15 @@
-// this is a test file
+class  Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert (key) {
+        // let newNode = new Node(key);
+
+        console.log(key)
+    }
+}
+
+
+let tree = new Tree();
+tree.insert('hello, world');
